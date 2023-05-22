cfe5b54bb54a70c45d17b6d5e3db97dd9aba9454
Denitsa Slavkova <deni9309@gmail.com>
2023-05-22 19:43:37 +0300

    Added create.hbs page

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -1,18 +1,31 @@
 const router = require('express').Router();
 
-const { getAll } = require('../services/accommodationService');
+const { getAll, getById } = require('../services/accommodationService');
 
 router.get('/', (req, res) => {
     const rooms = getAll();
 
     res.render('catalog', {
         title: 'All Accommodation',
-        rooms,
+        rooms
     });
-}); 
+});
 
 router.get('/:id', (req, res) => {
-    res.render('details', { title: 'Accommodation Details' });
-}); 
+    const roomId = req.params.id;
+    const room = getById(roomId);
+
+    if (room) {
+        res.render('details', {
+            title: 'Accommodation Details',
+            room
+        });
+    } else {
+        res.render('roomNotFound', {
+            title: 'Accommodation Details',
+            roomId
+        });
+    }
+});
 
 module.exports = router;
--- controllers/createController.js
+++ controllers/createController.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {  // path is defined in index.js
-    res.render('create', { title: 'Host New Accommodation' });
+    res.render('create', {
+        title: 'Host New Accommodation'
+    });
 });
 
 module.exports = router;
